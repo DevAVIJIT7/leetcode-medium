190f6811cd0eefd339959035d34b8138ac0cbde1
Avijit Majhi <avijit.majhi@bugcrowd.com>
2021-07-02 23:36:38 +0530

    Binary Search Tree Iterator

---
+++ stack/binary-search-tree-iterator.rb
@@ -0,0 +1,96 @@
+# Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+# BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+# boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+# int next() Moves the pointer to the right, then returns the number at the pointer.
+# Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+# You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+# Example 1:
+
+
+# Input
+# ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+# [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+# Output
+# [null, 3, 7, true, 9, true, 15, true, 20, false]
+
+# Explanation
+# BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+# bSTIterator.next();    // return 3
+# bSTIterator.next();    // return 7
+# bSTIterator.hasNext(); // return True
+# bSTIterator.next();    // return 9
+# bSTIterator.hasNext(); // return True
+# bSTIterator.next();    // return 15
+# bSTIterator.hasNext(); // return True
+# bSTIterator.next();    // return 20
+# bSTIterator.hasNext(); // return False
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+class BSTIterator
+
+=begin
+    :type root: TreeNode
+=end
+    def initialize(root)
+        @st = []
+        @arr = []
+        @i = 0
+        while root
+            @st << root
+            root = root.left
+        end
+
+        while !@st.empty?
+            x = @st.pop()
+            @arr << x.val
+            if x.right
+                @st << x.right
+                temp = x.right
+                if temp && temp.left
+                    while temp.left
+                        @st << temp.left
+                        temp = temp.left
+                    end
+                end   
+            end
+        end
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def next()
+        x = @arr[@i]
+        @i += 1
+        x
+    end
+
+
+=begin
+    :rtype: Boolean
+=end
+    def has_next()
+        @i <= @arr.length && @arr[@i] != nil
+    end
+
+
+end
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator.new(root)
+# param_1 = obj.next()
+# param_2 = obj.has_next()
