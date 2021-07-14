ff0b90a5ffae1378f239ee73e66c3f1d0e9766d5
Avijit Majhi <avijit777.dev@gmail.com>
2021-07-14 18:12:40 +0530

    greedy progrmaing from leetcode (#5)

---
+++ greedy/maximum_69_number.rb
@@ -0,0 +1,39 @@
+# Given a positive integer num consisting only of digits 6 and 9.
+
+# Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+ 
+
+# Example 1:
+
+# Input: num = 9669
+# Output: 9969
+# Explanation: 
+# Changing the first digit results in 6669.
+# Changing the second digit results in 9969.
+# Changing the third digit results in 9699.
+# Changing the fourth digit results in 9666. 
+# The maximum number is 9969.
+# Example 2:
+
+# Input: num = 9996
+# Output: 9999
+# Explanation: Changing the last digit 6 to 9 results in the maximum number.
+# Example 3:
+
+# Input: num = 9999
+# Output: 9999
+# Explanation: It is better not to apply any change.
+
+# @param {Integer} num
+# @return {Integer}
+def maximum69_number (num)
+  s = num.to_s
+  s.each_char.with_index do |c, i|
+      if c=='6'
+          s[i] = '9'
+          break
+      end
+  end
+  s.to_i
+end
